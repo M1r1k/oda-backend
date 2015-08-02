c2288c396aa47bc857b0a22de035dbb469c859d6
Artem Miroshnyk <artyom.miroshnik@propeople.com.ua>
2015-08-02 19:41:49 +0300

    Fix response formatter after symfony update

--- src/ODADnepr/MockServiceBundle/Controller/SecurityController.php
+++ src/ODADnepr/MockServiceBundle/Controller/SecurityController.php
@@ -2,24 +2,39 @@
 
 namespace ODADnepr\MockServiceBundle\Controller;
 
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class SecurityController extends Controller
 {
-    /**
-     * @Route("/rest/v1/user-auth")
-     * @Template()
-     */
-    public function loginAction()
-    {
-        return array(
-                // ...
-            );    }
 
     /**
+     * @ApiDoc(
+     *   resource=false,
+     *   description="Endpoint for user authorization through JWT",
+     *   requirements={
+     *     {
+     *       "name"="username",
+     *       "dataType"="string",
+     *       "description"="User name or email"
+     *     },
+     *     {
+     *       "name"="password",
+     *       "dataType"="string",
+     *       "description"="User password"
+     *     },
+     *   },
+     *   statusCodes={
+     *     200="Returned when authorization was successful",
+     *     401="Returned when Bad Credentials were given"
+     *   }
+     * )
+     *
      * @Route("/rest/v1/user-auth-check")
+     * @Method({"POST"})
      * @Template()
      */
     public function loginCheckAction()
