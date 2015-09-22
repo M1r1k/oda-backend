a448cc4df486013b77fa095ad890eaf6b365d93a
Artem Miroshnyk <artyom.miroshnik@propeople.com.ua>
2015-09-22 14:10:49 +0200

    Recreate Ticket form type

---
+++ src/ODADnepr/MockServiceBundle/Form/TicketType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ODADnepr\MockServiceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TicketType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fb_registered')
+            ->add('title')
+            ->add('body')
+            ->add('created_date')
+            ->add('start_date')
+            ->add('updated_date')
+            ->add('completed_date')
+            ->add('ticket_id')
+            ->add('comment')
+            ->add('user')
+            ->add('address')
+            ->add('geo_address')
+            ->add('manager')
+            ->add('category')
+            ->add('type')
+            ->add('state')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ODADnepr\MockServiceBundle\Entity\Ticket'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'odadnepr_mockservicebundle_ticket';
+    }
+}
